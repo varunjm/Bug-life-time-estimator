42f48fe9a81126df8549f45fe2bf10082653b668
Varun Jayathirtha <vjayath@ncsu.edu>
2016-11-29 01:03:17 -0500

    added more error checks

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     router = express.Router(),
     bot = require('../lib/bot.js');
+
 /* GET home page. */
 router.post('/predict-bug-lifetime', function(req, res, next) {
     bot.process_event(req.body);
